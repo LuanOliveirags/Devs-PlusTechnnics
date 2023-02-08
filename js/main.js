ad65abe35d63a7d957b50e939950eec123c2d067
Luan Oliveira <Luanoliveirags@gmail.com>
2023-02-07 22:09:57 -0300

    commit initial

--- js/main.js
+++ js/main.js
@@ -1,24 +1,10 @@
-// Smooth Scrolling
-$("#navbar a, .btn").on("click", function (event) {
-    if (this.hash !== "") {
-        event.preventDefault();
-
-        const hash = this.hash;
-
-        $("html, body").animate(
-            {
-                scrollTop: $(hash).offset().top - 100
-            },
-            800
-        );
-    }
-});
-
-// Sticky menu background
-window.addEventListener("scroll", function () {
-    if (window.scrollY > 150) {
-        document.querySelector("#navbar").style.opacity = 0.9;
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/iconMenu.png";
     } else {
-        document.querySelector("#navbar").style.opacity = 1;
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "img/iconMenu.png";
     }
-});
+}
